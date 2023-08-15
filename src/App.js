a00818e96e47b8d82b647e07a456bc6462edd37f
orlando <naijarace1@gmail.com>
2023-08-15 12:36:24 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Partners from "./components/Partners";
+import Category from "./components/Category";
+import Honeymoon from "./components/Honeymoon";
+import Booking from "./components/Booking";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Header />
+        <Banner />
+        <Partners />
+        <Category />
+        <Honeymoon />
+        <Booking />
+        <Footer />
+    </>
   );
 }
 
